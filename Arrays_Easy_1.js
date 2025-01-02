7694ed64970d7c60ac5c6eb67bb14776261376e4
ashishkalsi07 <ashishkalsi07@gmail.com>
2025-01-02 22:19:42 +0530

    Easy Problem 1

---
+++ Arrays_Easy_1.js
@@ -0,0 +1,13 @@
+const array = [2,2,3,4,9,9]
+
+function remove_Duplicates(arr){
+    const uniqueElements = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (!uniqueElements.includes(arr[i])) {
+            uniqueElements.push(arr[i]);
+        }
+    }
+    return uniqueElements;
+}
+let ans = remove_Duplicates(array)
+console.log(ans)
